6a2514be80c73e1d843846676fa629ef275fa098
Abd-mt <jasiel.19070045@itsmotul.edu.mx>
2023-04-12 15:53:17 -0500

    controller

--- instalacion-app/app/Models/asignatura.php
+++ instalacion-app/app/Models/asignatura.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Notas extends Model
+class asignatura extends Model
 {
     use HasFactory;
+    protected $fillable =[
+        'carrera',
+        'semestre',
+        'nombre',
+        'maestro'
+
+
+    ];
 }
---
+++ instalacion-app/database/migrations/2023_04_07_202737_create_usuarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombres', 75);
+            $table->string('apellidos', 75);
+            $table->string('nombreUsuario')->unique();
+            $table->string('matricula')->unique();
+            $table->string('carrera');
+            $table->dateTimeTz('horaCreacion', $precision = 0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
