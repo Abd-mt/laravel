cb61fbec44fd33247bab90893064787d79cc0066
Abd-mt <jasiel.19070045@itsmotul.edu.mx>
2023-04-15 19:43:33 -0600

    cod

--- instalacion-app/database/seeders/notaSeeders.php
+++ instalacion-app/database/seeders/notaSeeders.php
@@ -6,7 +6,7 @@ use App\Models\nota;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class notasSeeders extends Seeder
+class notaSeeders extends Seeder
 {
     /**
      * Run the database seeds.
